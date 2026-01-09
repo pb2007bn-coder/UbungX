41758cd7f14d096e64b867f71fd1fbe7f59242ad
pb2007bn-coder <pb2007bn@gmail.com>
2026-01-09 13:42:40 +0100

    Update Spielbrett.java

--- h2/Spielbrett.java
+++ h2/Spielbrett.java
@@ -35,31 +35,7 @@ public class Spielbrett {
   public Feld getFeld(int row, int col) {
 	    return brett[row][col];
 	}
-  public void printBrett(Spielstein spielstein) {
-	    for (int i = 0; i < dim; i++) {
-	        for (int j = 0; j < dim; j++) {
-	            Feld f = brett[i][j];
-	            String feldDarstellung = "";
-
-	            if (f.getBoese()) {
-	                feldDarstellung += "X";
-	            } else {
-	                feldDarstellung += " ";
-	            }
-
-	            feldDarstellung += f.getDirection();
-
-	            if (spielstein.getCurrentRow() == i && spielstein.getCurrentCol() == j) {
-	                feldDarstellung = "[" + feldDarstellung + "]";
-	            } else {
-	                feldDarstellung = " " + feldDarstellung + " ";
-	            }
-
-	            System.out.print(feldDarstellung);
-	        }
-	        System.out.println();
-	    }
-	}
+  
 
 
 }
